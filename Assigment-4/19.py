e5247e059b0d33f203119ea9d687a200b5c560cb
Anurag Tiwari <at956603@gmail.com>
2025-04-16 21:04:06 +0530

    19 and 25 updated

--- Assigment-4/19.py
+++ Assigment-4/19.py
@@ -1,1 +1,7 @@
 # 19. Remove Negative Numbers
+lst = [-1, 4, -5, 6]
+pos = []
+for i in lst:
+    if i >= 0:
+        pos.append(i)
+print(pos)
--- Assigment-4/25.py
+++ Assigment-4/25.py
@@ -1,1 +1,4 @@
 # 25. List of Tuples to Dictionary
+lst = [("a", 1), ("b", 2)]
+d = dict(lst)
+print(d)
